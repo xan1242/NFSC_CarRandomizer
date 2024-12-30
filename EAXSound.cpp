0261ad675b301e9cbef785fd6a63ab7b0a5c30cf
xan1242 <8014093+xan1242@users.noreply.github.com>
2024-12-30 03:42:01 +0100

    proper sound reload

--- EAXSound.cpp
+++ EAXSound.cpp
@@ -16,8 +16,15 @@ namespace EAXSound
 	//uintptr_t pInitializeInGame;
 	//uintptr_t pMixMapReadyCallback;
 	//uintptr_t pLoadCommonIngameFiles;
+	uintptr_t pCloseSound;
+	uintptr_t pUpdate;
+	uintptr_t pAreResourceLoadsPending;
+	uintptr_t pCheckQueuedFileCallbacks;
+	uintptr_t pStartQueuedFileReading;
 
-	//uintptr_t pUnloadAemsInGame;
+	uintptr_t pLoadAemsInGame;
+	uintptr_t pUnloadAemsInGame;
+	uintptr_t pIsSoundEnabled;
 
 	//uintptr_t p_gb_DORESTART_RACE;
 
@@ -93,13 +100,22 @@ namespace EAXSound
 	//	reinterpret_cast<void(__thiscall*)(uintptr_t, void(__cdecl*)(void*), void*)>(pLoadInGameSoundBanks)(instance, callback, callback_param);
 	//}
 	//
-	//static void UnloadAemsInGame()
-	//{
-	//	if (!pUnloadAemsInGame)
-	//		return;
-	//
-	//	reinterpret_cast<void(*)()>(pUnloadAemsInGame)();
-	//}
+	static void UnloadAemsInGame()
+	{
+		if (!pUnloadAemsInGame)
+			return;
+	
+		reinterpret_cast<void(*)()>(pUnloadAemsInGame)();
+	}
+
+	static void LoadAemsInGame(void(__cdecl* callback)(void*), void* callback_param)
+	{
+		if (!pLoadAemsInGame)
+			return;
+
+		reinterpret_cast<void(*)(void(__cdecl*)(void*), void*)>(pLoadAemsInGame)(callback, callback_param);
+	}
+
 	//
 	//static void TriggerAudioRestartRace()
 	//{
@@ -117,8 +133,51 @@ namespace EAXSound
 	//	reinterpret_cast<void(*)()>(pLoadCommonIngameFiles)();
 	//}
 
+	static void CloseSound()
+	{
+		if (!pCloseSound)
+			return;
+	
+		reinterpret_cast<void(*)()>(pCloseSound)();
+	}
+
+	static void Update(uintptr_t instance, float dT)
+	{
+		if (!pUpdate)
+			return;
+
+		reinterpret_cast<void(__thiscall*)(uintptr_t, float)>(pUpdate)(instance, dT);
+	}
+
+	static void CheckQueuedFileCallbacks()
+	{
+		if (!pCheckQueuedFileCallbacks)
+			return;
+
+		reinterpret_cast<void(*)()>(pCheckQueuedFileCallbacks)();
+	}
+
+	static void StartQueuedFileReading()
+	{
+		if (!pStartQueuedFileReading)
+			return;
+
+		reinterpret_cast<void(*)()>(pStartQueuedFileReading)();
+	}
+
+	static bool AreResourceLoadsPending(uintptr_t instance)
+	{
+		if (!pAreResourceLoadsPending)
+			return false;
+
+		reinterpret_cast<bool(__thiscall*)(uintptr_t)>(pAreResourceLoadsPending)(instance);
+	}
+
 	void Refresh()
 	{
+		if (pIsSoundEnabled && !*reinterpret_cast<bool*>(pIsSoundEnabled))
+			return;
+
 		uintptr_t pEAXSound = Get();
 		if (pEAXSound)
 		{
@@ -135,14 +194,23 @@ namespace EAXSound
 			// this is the only way to get the engine sounds working, but, it's highly unstable to do this after race finishes
 			//StartNewGamePlay(pEAXSound);
 
-			ReStartRace(pEAXSound, false);
+			//ReStartRace(pEAXSound, false);
+			CloseSound();
 
 			//EAXSound_ReStartRace(pEAXSound, false);
 			//TriggerAudioRestartRace();
 			//*reinterpret_cast<bool*>(0xA8BA77) = true;
 			//EAXSound_RefreshNewGameplay(pEAXSound);
 
-			//UnloadAemsInGame();
+			while (AreResourceLoadsPending(pEAXSound))
+			{
+				Update(pEAXSound, 0.1f);
+				CheckQueuedFileCallbacks();
+				StartQueuedFileReading();
+			}
+
+			UnloadAemsInGame();
+			LoadAemsInGame(nullptr, nullptr);
 			//EAXSound_LoadInGameSoundBanks(pEAXSound, PostSoundReload, reinterpret_cast<void*>(pEAXSound));
 		}
 	}
@@ -155,12 +223,19 @@ namespace EAXSound
 		//uintptr_t loc_51B38D = 0x51B38D;
 		//uintptr_t loc_6699D2 = 0x6699D2;
 		//uintptr_t loc_523549 = 0x523549;
-		//uintptr_t loc_6B7C89 = 0x6B7C89;
+		uintptr_t loc_6B7C89 = 0x6B7C89;
 		//uintptr_t loc_66C755 = 0x66C755;
 		//uintptr_t loc_52328F = 0x52328F;
 		uintptr_t loc_67DB9A = 0x67DB9A;
 		//uintptr_t loc_522157 = 0x522157;
 		//uintptr_t loc_522168 = 0x522168;
+		uintptr_t loc_6B5B09 = 0x6B5B09;
+		uintptr_t loc_6B7C6B = 0x6B7C6B;
+		uintptr_t loc_6B7C70 = 0x6B7C70;
+		uintptr_t loc_6B7C75 = 0x6B7C75;
+		uintptr_t loc_6B7C80 = 0x6B7C80;
+		uintptr_t loc_6BBA78 = 0x6BBA78;
+		uintptr_t loc_51F780 = 0x51F780;
 
 		//p_gb_DORESTART_RACE = *reinterpret_cast<uintptr_t*>(loc_522136 + 1);
 		ppEAXSound = *reinterpret_cast<uintptr_t*>(loc_65DB0C + 2);
@@ -172,7 +247,14 @@ namespace EAXSound
 		pReStartRace = static_cast<uintptr_t>(injector::GetBranchDestination(loc_67DB9A));
 		//pInitializeInGame = static_cast<uintptr_t>(injector::GetBranchDestination(loc_522157));
 		//pMixMapReadyCallback = static_cast<uintptr_t>(injector::GetBranchDestination(loc_522168));
-		//pUnloadAemsInGame = static_cast<uintptr_t>(injector::GetBranchDestination(loc_6B7C89));
+		pUnloadAemsInGame = static_cast<uintptr_t>(injector::GetBranchDestination(loc_6B7C89));
+		pLoadAemsInGame = static_cast<uintptr_t>(injector::GetBranchDestination(loc_6BBA78));
 		//pLoadCommonIngameFiles = static_cast<uintptr_t>(injector::GetBranchDestination(loc_52328F));
+		pCloseSound = static_cast<uintptr_t>(injector::GetBranchDestination(loc_6B5B09));
+		pUpdate = static_cast<uintptr_t>(injector::GetBranchDestination(loc_6B7C6B));
+		pCheckQueuedFileCallbacks = static_cast<uintptr_t>(injector::GetBranchDestination(loc_6B7C70));
+		pStartQueuedFileReading = static_cast<uintptr_t>(injector::GetBranchDestination(loc_6B7C75));
+		pAreResourceLoadsPending = static_cast<uintptr_t>(injector::GetBranchDestination(loc_6B7C80));
+		pIsSoundEnabled = *reinterpret_cast<uintptr_t*>(loc_51F780 + 1);
 	}
 }
